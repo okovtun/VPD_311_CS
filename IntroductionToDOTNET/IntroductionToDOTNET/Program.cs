478c300c9c4ee2ea52f0baa85a7a344166b4c6fe
User <User@DESKTOP-0TCDSR6>
2024-07-21 16:20:54 +0300

    Intro

---
+++ IntroductionToDOTNET/IntroductionToDOTNET/Program.cs
@@ -0,0 +1,43 @@
+﻿//#define CONSOLE_PARAMETERS
+//#define INPUT_DATA
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToDOTNET
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "Introduction to .NET";
+#if CONSOLE_PARAMETERS
+			Console.Beep(137, 1000);
+			Console.BackgroundColor = ConsoleColor.DarkBlue;
+			//Console.CursorLeft = 22;
+			//Console.CursorTop = 11;
+			Console.SetCursorPosition(22, 7);
+			Console.Write("Hello .NET\n");    //Выводит строку на экран
+			Console.WriteLine("Introduction");//Выводит строку на экран, и переводит курсор в начало следующей строки.
+			Console.BackgroundColor = ConsoleColor.DarkGreen;
+			Console.ResetColor(); 
+#endif
+
+#if INPUT_DATA
+			Console.Write("Введите Ваше имя: ");
+			string first_name = Console.ReadLine();
+			Console.Write("Введите Вашу фамилию: ");
+			string last_name = Console.ReadLine();
+			Console.Write("Введите Ваш возраст:");
+			int age = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Ваше имя: " + first_name + ", Ваша фамилия: " + last_name + ", Ваш возраст: " + age);   //Конкатенация строк
+			Console.WriteLine(String.Format("Ваше имя: {0}, Ваша фамилия: {1}, Ваш возраст: {2}", first_name, last_name, age));//Форматирование строк
+			Console.WriteLine($"Ваше имя: {first_name}, Ваша фамилия: {last_name}, Ваш возраст: {age}");    //Интерполяция строк  
+#endif
+			
+		}
+	}
+}
---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+﻿https://github.com/okovtun/VPD_311_CS.git
+
+TODO:
+1. https://github.com/okovtun/BV_011/blob/master/ControlStructures/Geometria/Task/Geometria1.txt
+2. https://github.com/okovtun/BV_011/blob/master/ControlStructures/Geometria/Task/ChessBoard.jpg
+3. https://github.com/okovtun/BV_011/blob/master/ControlStructures/Geometria/Task/HardChess.jpg
+4. Написать программу, которая перемещает квадратик по экрану, при нажатии клавиш WASD, или соответствующих стрелок;
+5. Написать калькулятор, аналогичный тому, который писали на C++;
