c569547600ef83a75cbd1f725ca7480d58ff7b28
User <User@DESKTOP-0TCDSR6>
2024-10-06 21:22:07 +0300

    BinaryTree start

---
+++ DataContainers/BinaryTree/main.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+class Tree
+{
+	class Element
+	{
+		int Data;
+		Element* pLeft;
+		Element* pRight;
+	public:
+		Element(int Data, Element* pLeft = nullptr, Element* pRight = nullptr)
+			:Data(Data), pLeft(pLeft), pRight(pRight)
+		{
+			cout << "EConstructor:\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+		friend class Tree;
+	}*Root;
+public:
+	Element* getRoot()const
+	{
+		return Root;
+	}
+	Tree()
+	{
+		Root = nullptr;
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Tree()
+	{
+		cout << "TDestructor:\t" << this << endl;
+	}
+
+	void insert(int Data, Element* Root)
+	{
+		if (this->Root == nullptr)this->Root = new Element(Data);
+		if (Root == nullptr)return;
+		if (Data < Root->Data)
+		{
+			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			else insert(Data, Root->pLeft);
+		}
+		else
+		{
+			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
+			else insert(Data, Root->pRight);
+		}
+	}
+
+	//					Methods:
+	void print(Element* Root)const
+	{
+		if (Root == nullptr)return;
+		print(Root->pLeft);
+		cout << Root->Data << tab;
+		print(Root->pRight);
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "¬ведите размер дерева: "; cin >> n;
+	Tree tree;
+	for (int i = 0; i < n; i++)
+	{
+		tree.insert(rand() % 100, tree.getRoot());
+	}
+	tree.print(tree.getRoot());
+}
